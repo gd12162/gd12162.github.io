028b1086d1c6087f0e0aa08b3c087db0f2be81ad
gd12162 <greatgd93@gmail.com>
2025-09-14 22:18:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'v4a-cache-root-v1';
-const PRECACHE_URLS = ['/', '/index.html', '/manifest.webmanifest'];
+const CACHE_NAME = 'v4a-cache-root-v2';
+const PRECACHE_URLS = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)));
